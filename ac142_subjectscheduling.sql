5e514840bd772de952ba09379cacb3232e2b8057
ririlveria <rajahrilveria@gmail.com>
2015-12-14 12:46:59 +0800

    Update ac142_subjectscheduling.sql

--- ac142_subjectscheduling.sql
+++ ac142_subjectscheduling.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 14, 2015 at 05:37 AM
+-- Generation Time: Dec 14, 2015 at 05:47 AM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -48,6 +48,46 @@ CREATE TABLE IF NOT EXISTS `yearlevel` (
   `status` varchar(45) NOT NULL,
   `order` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `section`
+--
+ALTER TABLE `section`
+  ADD PRIMARY KEY (`id`), ADD KEY `yearlevel_id` (`yearlevel_id`);
+
+--
+-- Indexes for table `yearlevel`
+--
+ALTER TABLE `yearlevel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `section`
+--
+ALTER TABLE `section`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `yearlevel`
+--
+ALTER TABLE `yearlevel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `section`
+--
+ALTER TABLE `section`
+ADD CONSTRAINT `section_ibfk_1` FOREIGN KEY (`yearlevel_id`) REFERENCES `yearlevel` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
